c940648db8693335717fa7d7ccae71a88f219941
방정훈 <wjdgns2751@naver.com>
2024-02-28 19:59:13 +0900

    페이지 전환 컨트롤러

---
+++ src/main/java/com/bjh/review/domain/member/api/PageController.java
@@ -0,0 +1,45 @@
+package com.bjh.review.domain.member.api;
+
+import com.bjh.review.domain.member.application.MemberService;
+import com.bjh.review.domain.member.domain.Member;
+import com.bjh.review.domain.member.dto.MemberResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class PageController {
+    private final MemberService memberService;
+
+    @GetMapping("/")
+    public String mainPage(Model model){
+        MemberResponse.SignInDTO member = memberService.sessionCheck();
+        model.addAttribute("email", member.getEmail());
+        model.addAttribute("role", member.getRole());
+        return "main";
+    }
+
+    @GetMapping("/signIn")
+    public String signInPage(){
+        return "signin";
+    }
+
+    @GetMapping("/signUp")
+    public String signupPage(){
+        return "signup";
+    }
+
+    @GetMapping("/signOut")
+    public String signOutPage(HttpServletRequest req, HttpServletResponse res){
+        memberService.signOut(req,res);
+        return "redirect:/";
+    }
+
+
+}
