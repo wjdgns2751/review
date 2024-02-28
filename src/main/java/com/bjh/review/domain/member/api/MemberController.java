412083fedc9710e5b7ebb35f947d626913a00e3c
방정훈 <wjdgns2751@naver.com>
2024-02-28 19:59:53 +0900

    회원 관련 기능

--- src/main/java/com/bjh/review/domain/member/api/MemberController.java
+++ src/main/java/com/bjh/review/domain/member/api/MemberController.java
@@ -1,32 +1,25 @@
 package com.bjh.review.domain.member.api;
 
 import com.bjh.review.domain.member.application.MemberService;
-import com.bjh.review.domain.member.dto.SignUpRequestDTO;
+import com.bjh.review.domain.member.dto.MemberRequest;
+import com.bjh.review.domain.member.dto.MemberResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@Slf4j
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-    @PostMapping("/signup")
-    public ResponseEntity<String> signUp(@RequestBody SignUpRequestDTO signUpRequestDTO, HttpServletRequest request){
-        try {
-            //DB 등록
-            memberService.signUp(signUpRequestDTO);
 
-            //세션 등록
-
-            return ResponseEntity.ok("회원가입이 성공적으로 완료되었습니다.");
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
+    @PostMapping("/signUpProc")
+    public String signUp(MemberRequest.SignUpDTO signUpDTO, HttpServletRequest request) throws BadRequestException {
+        MemberResponse.SignUpDTO signDto = memberService.signUp(signUpDTO);
+        return "redirect:/login";
     }
-
 
 }
--- src/main/java/com/bjh/review/domain/member/application/MemberService.java
+++ src/main/java/com/bjh/review/domain/member/application/MemberService.java
@@ -1,47 +1,85 @@
 package com.bjh.review.domain.member.application;
 import com.bjh.review.domain.member.domain.Member;
-import com.bjh.review.domain.member.dto.SignUpRequestDTO;
+import com.bjh.review.domain.member.domain.Role;
+import com.bjh.review.domain.member.dto.MemberResponse;
+import com.bjh.review.domain.member.dto.MemberRequest;
 import com.bjh.review.domain.member.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Iterator;
+
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     /*
     * 회원 가입
     */
-    public void signUp(SignUpRequestDTO signUpRequestDTO) {
+    public MemberResponse.SignUpDTO signUp(MemberRequest.SignUpDTO sign) throws BadRequestException {
 
-        if (!isEmailAvailable(signUpRequestDTO.email()))
-            throw new RuntimeException("이미 사용중 인 아이디 입니다.");
+        if (!isEmailAvailable(sign.getEmail()))
+            throw new BadRequestException("이미 사용중 인 아이디 입니다.");
 
-        SignUpRequestDTO updatedSignUpRequestDTO = SignUpRequestDTO.builder()
-                .name(signUpRequestDTO.name())
-                .email(signUpRequestDTO.email())
-                .password(passwordEncoder.encode(signUpRequestDTO.password()))
-                .addr(signUpRequestDTO.addr())
+        MemberRequest.SignUpDTO updatedSignUpDTO = MemberRequest.SignUpDTO.builder()
+                .name(sign.getName())
+                .email(sign.getEmail())
+                .password(bCryptPasswordEncoder.encode(sign.getPassword()))
+                .addr(sign.getAddr())
                 .build();
 
-        Member member = updatedSignUpRequestDTO.toMemberEntity(signUpRequestDTO);
-        memberRepository.save(member);
+        Member member =  memberRepository.save(updatedSignUpDTO.toMemberEntity());
 
+        return new MemberResponse.SignUpDTO(member);
     }
 
 
     /*
      * 회원 가입 - 아이디 사용 가능 확인
      */
-    public boolean isEmailAvailable(String email){
+    private boolean isEmailAvailable(String email){
         return memberRepository.findMemberByEmail(email) == null;
     }
 
 
+    /*
+     * 회원 로그인 - 세션 정보 확인
+     * */
+    public MemberResponse.SignInDTO sessionCheck(){
+       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+       Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+       Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+       GrantedAuthority auth = iter.next();
 
+       String id = SecurityContextHolder.getContext().getAuthentication().getName();
+       String role = auth.getAuthority();
+
+       return new MemberResponse.SignInDTO(id,role);
+
+    }
+    /*
+     * 회원 로그아웃
+     * */
+    public void signOut(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+    }
 }
 
 
