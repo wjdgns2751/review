6d6d82af6daf1d92ed2896d6dd6889783f381054
방정훈 <wjdgns2751@naver.com>
2024-02-28 19:58:46 +0900

    레디스 설정

--- src/main/java/com/bjh/review/ReviewApplication.java
+++ src/main/java/com/bjh/review/ReviewApplication.java
@@ -2,10 +2,9 @@ package com.bjh.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
 public class ReviewApplication {
 
 	public static void main(String[] args) {
